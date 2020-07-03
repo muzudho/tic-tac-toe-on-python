536df2eeb69be3faaad8f3b6b87a2cf5332cd11d
むずでょ <muzudho1@gmail.com>
2020-07-03 22:10:22 +0900

    info_backward

--- src/look_and_model.py
+++ src/look_and_model.py
@@ -274,3 +274,31 @@ class Search():
         comment_str = f' {friend_str} "{comment}"' if comment != "" else ""
         log.print(
             f'info nps {nps: >6} nodes {nodes: >6} pv {pv: <17} | {friend_str} [{addr}] | .       {height} |       |{result_str}|{comment_str}\n')
+
+    def info_backward(self, pos: "Position", addr: int, result: "GameResult", comment: str, log: "Log"):
+        """後ろ向き探索のときの表示だぜ☆（＾～＾）
+        >>> from log import Log
+        >>> from look_and_model import Position, Search
+        >>> log = Log()
+        >>> pos = Position()
+        >>> search.info_backward(pos, 1, GameResult.WIN, 'Hello!', log)
+        info nps      0 nodes      0 pv                   |       | <- from height 0 | + [1] | win  | + "Hello!"
+
+        開発中は nps はまだ未実装だろうから 0 にでもしておけだぜ☆（＾～＾）
+        """
+        nps = 0  # TODO self.nps()
+        nodes = self.nodes
+        pv = self.pv(pos)
+        height = 'none    ' if SQUARES_NUM < pos.pieces_num else f'height {pos.pieces_num}'
+        friend_str = '+' if pos.friend == self.start_friend else '-'
+        if result == GameResult.WIN:
+            result_str = ' win  '
+        elif result == GameResult.DRAW:
+            result_str = ' draw '
+        elif result == GameResult.LOSE:
+            result_str = ' lose '
+        else:
+            raise ValueError(f'Invalid GameResult={result}')
+        comment_str = f' {friend_str} "{comment}"' if comment != "" else ""
+        log.print(
+            f'info nps {nps: >6} nodes {nodes: >6} pv {pv: <17} |       | <- from {height} | {friend_str} [{addr}] |{result_str}|{comment_str}\n')
