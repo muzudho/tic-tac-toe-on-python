e77c7689803d88de9f8d71ac9736bec965b756fe
むずでょ <muzudho1@gmail.com>
2020-07-03 20:12:48 +0900

    大変☆（＾～＾）

--- src/look_and_model.py
+++ src/look_and_model.py
@@ -6,8 +6,10 @@ class Piece(Enum):
     >>> from log import Log
     >>> from look_and_model import Piece
     >>> log = Log()
-    >>> log.print(f'Nought={Piece.NOUGHT}')
-    >>> log.print(f'Cross ={Piece.CROSS}')
+    >>> log.print(f'Nought=|{Piece.NOUGHT}|')
+    Nought=|o|
+    >>> log.print(f'Cross =|{Piece.CROSS}|')
+    Cross =|x|
     """
 
     # 文字もセットできるんだが、0を除く自然数を入れるほうが一般的かだぜ☆（＾～＾）？
@@ -23,4 +25,37 @@ class Piece(Enum):
         elif self.value == 2:
             return 'x'
         else:
-            raise ValueError(f'Invalid piece=|{self.value}|')
+            raise ValueError(f'Invalid Piece=|{self.value}|')
+
+
+class GameResult(Enum):
+    """〇×ゲームは完全解析できるから、評価ではなくて、ゲームの結果が分かるんだよな☆（＾～＾）
+    >>> from log import Log
+    >>> from look_and_model import GameResult
+    >>> log = Log()
+    >>> log.print(f'Win =|{GameResult.WIN}|')
+    Win   =|win|
+    >>> log.print(f'Draw=|{GameResult.DRAW}|')
+    Draw  =|draw|
+    >>> log.print(f'Lose=|{GameResult.LOSE}|')
+    Lose  =|lose|
+    """
+
+    # 勝ち☆（＾～＾）
+    WIN = 1
+
+    # 引き分け☆（＾～＾）
+    DRAW = 2
+
+    # 負け☆（＾～＾）
+    LOSE = 3
+
+    def __str__(self):
+        if self.value == 1:
+            return 'win'
+        elif self.value == 2:
+            return 'draw'
+        elif self.value == 3:
+            return 'lose'
+        else:
+            raise ValueError(f'Invalid GameResult=|{self.value}|')
--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 from log import Log
-from look_and_model import Piece
+from look_and_model import Piece, GameResult
 
 log = Log()
 log.clear()
 log.print("Hello!\n")
 log.write("World!\n")
 
-log.print(f'Nought={Piece.NOUGHT}')
-log.print(f'Cross ={Piece.CROSS}')
+log.print(f'Nought=|{Piece.NOUGHT}|')
+log.print(f'Cross =|{Piece.CROSS}|')
+log.print(f'Win   =|{GameResult.WIN}|')
+log.print(f'Draw  =|{GameResult.DRAW}|')
+log.print(f'Lose  =|{GameResult.LOSE}|')
